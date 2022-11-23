334b075e445164709f90078b7304f4a2f2ebafbe
Alireza Mirhosseini <shapourdisco78@gmail.com>
2022-11-23 11:36:53 +0330

    Finish work with database

--- Work-with-DB-Firebase/src/App.js
+++ Work-with-DB-Firebase/src/App.js
@@ -17,16 +17,19 @@ function App() {
                 throw new Error('Something went wrong!');
             }
             const data = await response.json();
-    
-            const transformedMovies = data.results.map(movieData => {
-                return {
-                    id: movieData.episode_id,
-                    title: movieData.title,
-                    openingText: movieData.opening_crawl,
-                    releaseDate: movieData.release_date
-                };
-            });
-            setMovies(transformedMovies);
+
+            const loadedMovies = [];
+
+            for (const key in data) {
+                loadedMovies.push({
+                    id: key,
+                    title: data[key].title,
+                    openingText: data[key].openingText,
+                    releaseDate: data[key].releaseDate
+                })
+            }
+            
+            setMovies(loadedMovies);
         } catch (error) {
             setError(error.message);
         }
@@ -37,8 +40,16 @@ function App() {
         fetchMoviesHandler();
     }, [fetchMoviesHandler]);
 
-    function addMovieHandler(movie) {
-        fetch();
+    async function addMovieHandler(movie) {
+        const response = await fetch('https://react-http-a75c2-default-rtdb.firebaseio.com/movies.json', {
+            method: 'POST',
+            body: JSON.stringify(movie),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
     }
 
     let content = <p>Found no movies.</p>;
@@ -60,10 +71,10 @@ function App() {
             <section>
                 <AddMovie onAddMovie={addMovieHandler} />
             </section>
-        <section>
-            <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-        </section>
-        <section>{content}</section>
+            <section>
+                <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+            </section>
+            <section>{content}</section>
         </React.Fragment>
     );
 }
