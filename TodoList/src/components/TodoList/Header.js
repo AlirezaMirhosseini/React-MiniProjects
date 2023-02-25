62f0416e4736565d01efbe48ee91ab03086b099e
Alireza Mirhosseini <shapourdisco78@gmail.com>
2023-02-25 22:12:37 +0330

    complete TodoList

--- TodoList/src/components/TodoList/Header.js
+++ TodoList/src/components/TodoList/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
         return (
 
             <header>
-                <h1>SabzLearn Todo List</h1>
+                <h1>Todo List</h1>
             </header>
 
         )
